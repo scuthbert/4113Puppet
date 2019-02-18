6f6151fba580d15fc132f1af8f981246b2936435
root <root@machinea.dundermifflin.com>
2019-02-18 13:10:00 -0700

    Deleting test, adding changes to cron module.

--- code/modules/cron_puppet/manifests/init.pp
+++ code/modules/cron_puppet/manifests/init.pp
@@ -1,48 +1,20 @@
 # Class: cron_puppet
 # ===========================
-#
-# Full description of class cron_puppet here.
-#
-# Parameters
-# ----------
-#
-# Document parameters here.
-#
-# * `sample parameter`
-# Explanation of what this parameter affects and what it defaults to.
-# e.g. "Specify one or more upstream ntp servers as an array."
-#
-# Variables
-# ----------
-#
-# Here you should define a list of variables that this module would require.
-#
-# * `sample variable`
-#  Explanation of how this variable affects the function of this class and if
-#  it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#  External Node Classifier as a comma separated list of hostnames." (Note,
-#  global variables should be avoided in favor of class parameters as
-#  of Puppet 2.6.)
-#
-# Examples
-# --------
-#
-# @example
-#    class { 'cron_puppet':
-#      servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#    }
-#
-# Authors
-# -------
-#
-# Author Name <author@domain.com>
-#
-# Copyright
-# ---------
-#
-# Copyright 2019 Your name here, unless otherwise noted.
-#
+
 class cron_puppet {
-
-
+    file { 'post-hook':
+        ensure  => file,
+        path    => '/etc/puppetlabs/.git/hooks/post-merge',
+        source  => 'puppet:///modules/cron_puppet/post-merge',
+        mode    => "0755",
+        owner   => root,
+        group   => root,
+    }
+    cron { 'puppet-apply':
+        ensure  => present,
+        command => "cd /etc/puppetlabs; git pull",
+        user    => root,
+        minute  => '*/30',
+        require => File['post-hook'],
+    }
 }
