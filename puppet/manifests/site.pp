919bd2b6c9e3dd0d862be093e0df58cf687b6949
root <root@machinea.dundermifflin.com>
2019-02-26 10:02:59 -0700

    Adding final manifest

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -7,6 +7,20 @@ node default {
         'password    requisite      pam_pwquality.so try_first_pass local_users_only retry=3 minlen=10 dcredit=-2 ucredit=-2 minclass=4 authtok_type=',
         ],
     }
+
+    file_line { 'profile_umask':
+        ensure => present,
+        path   => '/etc/profile',
+        line   => 'umask 007',
+        match  => '\s*umask\s002$',
+    }
+
+    file_line { 'bash_umask':
+        ensure => present,
+        path   => '/etc/bashrc',
+        line   => 'umask 007',
+        match  => '\s*umask\s002$',
+    }
 }
 
 node 'machinec' {
@@ -28,6 +42,21 @@ node 'machinec' {
     sudo::conf { 'mpalmer-vsftpd':
         priority => 60,
     	content => 'mpalmer ALL=(ALL) /usr/bin/systemctl restart vsftpd.service'
+    }
+
+
+    file_line { 'profile_umask':
+        ensure => present,
+        path   => '/etc/profile',
+        line   => 'umask 007',
+        match  => '\s*umask\s002$',
+    }
+
+    file_line { 'bash_umask':
+        ensure => present,
+        path   => '/etc/bashrc',
+        line   => 'umask 007',
+        match  => '\s*umask\s002$',
     }
 }
 
