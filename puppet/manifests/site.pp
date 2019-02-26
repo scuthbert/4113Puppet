42e9fc8d8334071d8b9f184765e49fb4f29ef8a2
root <root@machinea.dundermifflin.com>
2019-02-26 10:01:56 -0700

    Adding final manifest

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,6 +1,12 @@
 node default {
     include cron_puppet
     include dm_employees
+
+    class { 'pam':
+    pam_password_lines => [
+        'password    requisite      pam_pwquality.so try_first_pass local_users_only retry=3 minlen=10 dcredit=-2 ucredit=-2 minclass=4 authtok_type=',
+        ],
+    }
 }
 
 node 'machinec' {
@@ -13,7 +19,10 @@ node 'machinec' {
         'root',
         'mpalmer',
         'mscott',
-	    ]
+	    ],
+    pam_password_lines => [
+        'password    requisite      pam_pwquality.so try_first_pass local_users_only retry=3 minlen=10 dcredit=-2 ucredit=-2 minclass=4 authtok_type=',
+        ],
     }
 
     sudo::conf { 'mpalmer-vsftpd':
@@ -32,9 +41,12 @@ node 'machineb' {
         'root',
         'web',
         'mscott',
-	    ]
+	    ],
+    pam_password_lines => [
+        'password    requisite      pam_pwquality.so try_first_pass local_users_only retry=3 minlen=10 dcredit=-2 ucredit=-2 minclass=4 authtok_type=',
+        ],
     }
-    
+
     sudo::conf { 'web-httpd':
         priority => 60,
     	content => '%web ALL=(ALL) /usr/bin/systemctl restart httpd.service'
@@ -51,7 +63,25 @@ node 'machinea', 'machined' {
 	    'wheel', 
         'root',
         'mscott',
-	    ]
+	    ],
+    pam_password_lines => [
+        'password    requisite      pam_pwquality.so try_first_pass local_users_only retry=3 minlen=10 dcredit=-2 ucredit=-2 minclass=4 authtok_type=',
+        ],
     }
+
+    file_line { 'profile_umask':
+        ensure => present,
+        path   => '/etc/profile',
+        line   => 'umask 007',
+        match  => '\s*umask\s002$',
+    }
+
+    file_line { 'bash_umask':
+        ensure => present,
+        path   => '/etc/bashrc',
+        line   => 'umask 007',
+        match  => '\s*umask\s002$',
+    }
+
 }
 
