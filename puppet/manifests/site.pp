0c7c85f47d1cfcfbc00a72563ebf5d52365d3eaa
root <root@machinea.dundermifflin.com>
2019-03-17 16:46:09 -0600

    Updating manifest

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -19,11 +19,34 @@ node 'machinea' {
         family    => 'inet',
         ipaddress => '100.64.0.18',
         method    => 'static',
-        netmask   => '255.255.0.0',
-        gateway   => '100.64.0.254',
+        netmask   => '255.255.255.0',
         onboot    => 'true',
         hotplug   => 'true',
     }
+
+    class { 'dhcp':
+	dnsdomain      => [
+    	    'dundermifflin.com',
+    	],
+        service_ensure => running,
+        nameservers  => ['8.8.8.8'],
+        ntpservers   => ['us.pool.ntp.org'],
+        interfaces   => ['ens192'],
+    }
+
+    dhcp::pool{ 'dundermifflin.com':
+  	network => '100.64.18.0',
+  	mask    => '255.255.255.0',
+  	range   => '100.64.18.6 100.64.18.255',
+  	gateway => '100.64.0.254',
+    }
+
+    dhcp::host { 'carriage':
+        mac     => '00:50:56:b4:d2:15',
+        ip      => '100.64.18.2',
+    }
+
+
 }
 
 node 'machineb' {
